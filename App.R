050e7c6c00ba3977e24a983ea589a8555e97316c
unknown <aubry.florient@outlook.fr>
2025-09-18 18:02:18 +0200

    v1

---
+++ App.R
@@ -0,0 +1,174 @@
+library(shiny)
+library(quantmod)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(highcharter)
+library(DT)
+library(data.table)
+
+ticker             = "CS.PA"
+start_date         = "1990-01-01"
+end_date           = "2025-09-01"
+investment_period  = 5
+initial_investment = 100
+decote_classique   = 0.2
+decote_garantie    = 0.064
+
+read_data <<- FALSE
+source('Functions.R')
+
+
+ui <- fluidPage(
+  titlePanel("Comparaison des Formules Shareplan 2025"),
+  sidebarLayout(
+    sidebarPanel(width = 2,
+                 textInput("ticker", "Ticker (dummy input)", value = ticker),
+                 dateInput("global_start","Date début graphique", value = "2007-01-01" ,
+                           min = start_date, max = ymd(end_date) - years(5)),
+                 numericInput("investment_period", "Durée détention (années)", value = investment_period, min = 1),
+                 numericInput("initial_investment", "Capital initial (€)", value = initial_investment, min = 1),
+                 numericInput("decote_classique", "Décote formule classique", value = decote_classique, min = 0, max = 1, step = 0.01),
+                 numericInput("decote_garantie", "Décote formule garantie", value = decote_garantie, min = 0, max = 1, step = 0.001),
+                 actionButton("run", "Lancer la comparaison historique"),
+    ),
+    mainPanel(width = 10,
+              fluidRow(
+                column(12,
+                       wellPanel(
+                         h3("📈 Payoff pour la date de départ spécifiée"),
+                         tabPanel("Graph - Comparaison", highchartOutput("hc_full", height = "600px")),
+                         tabPanel("Graph - Classique", highchartOutput("hc_classique", height = "600px"))
+                       )
+                )
+              ),
+              fluidRow(
+                column(12,
+                       wellPanel(
+                         h3("💰 Payoff pour plusieurs dates de départ (investissement initial de 100€)"),
+                         tabPanel("Graph - Payoffs hebdos", highchartOutput("hc_payoffs", height = "600px")),
+                         tabPanel("Table - Payoffs", DTOutput("tbl_payoffs"))
+                       )
+                )
+              )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  output$hc_payoffs <- renderHighchart({
+    comp <- comp_data
+    hc <- highchart() %>%
+      hc_add_series(comp, "line", hcaes(x = StartDate, y = Classique_TotalValue), name = "Classique") %>%
+      hc_add_series(comp, "line", hcaes(x = StartDate, y = Garantie_Employee),   name = "Garantie (employé)") %>%
+      hc_add_series(comp, "line", hcaes(x = StartDate, y = Garantie_TotalValue), name = "Garantie (total)", dashStyle = "ShortDot") %>%
+      hc_title(text = "Payoff final en fonction de la date de départ (pas annuel)") %>%
+      hc_yAxis(title = list(text = "Valeur (€)")) %>%
+      hc_xAxis(type = "datetime") %>%
+      hc_tooltip(shared = TRUE, pointFormat = "{series.name}: {point.y:.2f} €")
+    hc
+  })
+  
+  
+  simulations <- eventReactive(input$run, {
+    list(
+      simulation_1 = simulate_investment_immediate(stock_data, dividend_data,
+                                                   initial_investment = input$initial_investment, input$global_start, input$investment_period, 
+                                                   reinvest = TRUE,
+                                                   decote   = input$decote_classique),
+      
+      simulation_2 = simulate_investment_immediate(stock_data, dividend_data,
+                                                   initial_investment = input$initial_investment, input$global_start, input$investment_period,
+                                                   reinvest = TRUE, decote   = 0),
+      
+      simulation_3 = simulate_investment_immediate(stock_data, dividend_data,
+                                                   initial_investment = input$initial_investment, input$global_start, input$investment_period,
+                                                   reinvest = FALSE, decote   = 0),
+      
+      simulate_investment_garantie_ =  simulate_investment_garantie(stock_data, dividend_data,
+                                                                    initial_investment = input$initial_investment, input$global_start, input$investment_period, 
+                                                                    reinvest = TRUE,
+                                                                    decote   = input$decote_garantie)
+      
+    )
+  })
+  
+  output$hc_full <- renderHighchart({
+    sims <- simulations()  
+    
+    hc <- highchart() %>%
+      hc_add_series(sims$simulate_investment_garantie_, "line", hcaes(x = Date, y = TotalValueTotal),
+                    name = "Garantie - Payoff total (salarie + banque)") %>%
+      hc_add_series(sims$simulate_investment_garantie_, "line", hcaes(x = Date, y = TotalValueEmployee),
+                    name = "Garantie - Payoff salarie") %>%
+      hc_add_series(sims$simulate_investment_garantie_, "line", hcaes(x = Date, y = TotalValueTotal - TotalValueEmployee),
+                    name = "Garantie - Payoff banque (decote + dividendes)", dashStyle = "ShortDot") %>%
+      hc_add_series(sims$simulation_1, "line", hcaes(x = Date, y = TotalValue),
+                    name = "Classique - Valeur totale (réinvesti + décote)") %>%
+      hc_title(text = "Évolution de la valeur totale et des changements d’actions") %>%
+      hc_yAxis(title = list(text = "Valeur (€)")) %>%
+      hc_xAxis(type = "datetime", title = list(text = "Date")) %>%
+      hc_legend(enabled = TRUE, layout = "vertical", align = "right", verticalAlign = "top",
+                borderWidth = 1, backgroundColor = "#FFFFFF") %>%
+      hc_tooltip(shared = TRUE,
+                 pointFormat = "Valeur: {point.y:.2f} €")
+    hc
+    
+  })
+  
+  output$hc_classique <- renderHighchart({
+    sims <- simulations()  
+    
+    shares_changes <- sims$simulation_1 %>%
+      mutate(Change = Shares != lag(Shares, default = first(Shares))) %>%
+      filter(Change) %>%
+      select(Date, Shares, TotalValue)
+
+    shares_changes = rbind(sims$simulation_1[1,c('Date','Shares','TotalValue')], shares_changes)
+
+    # créer les plotLines pour chaque changement
+    plot_lines <- lapply(1:nrow(shares_changes), function(i) {
+      list(
+        value = datetime_to_timestamp(as.POSIXct(shares_changes$Date[i])),
+        color = "#7cb5ec",
+        width = 2,
+        dashStyle = "ShortDash",
+        label = list(
+          text = paste0("Actions: ", round(shares_changes$Shares[i], 2)),
+          rotation = 0,
+          align = "left",
+          style = list(color = "#7cb5ec", fontSize = "10px")
+        )
+      )
+    })
+
+    # graphique Highcharter
+    hc <- highchart() %>%
+      hc_add_series(sims$simulation_1, "line", hcaes(x = Date, y = TotalValue),
+                    name = "Valeur totale (réinvesti + décote)") %>%
+      hc_add_series(sims$simulation_2, "line", hcaes(x = Date, y = TotalValue),
+                    name = "Valeur totale (réinvesti + sans décote)") %>%
+      hc_add_series(sims$simulation_3, "line", hcaes(x = Date, y = TotalValue),
+                    name = "Valeur totale (non réinvesti + sans décote)", dashStyle = "ShortDot") %>%
+      hc_title(text = "Évolution de la valeur totale et des changements d’actions") %>%
+      hc_yAxis(title = list(text = "Valeur (€)")) %>%
+      hc_xAxis(type = "datetime", title = list(text = "Date"), plotLines = plot_lines) %>%
+      hc_legend(enabled = TRUE, layout = "vertical", align = "right", verticalAlign = "top",
+                borderWidth = 1, backgroundColor = "#FFFFFF") %>%
+      hc_tooltip(shared = TRUE,
+                 pointFormat = "Valeur: {point.y:.2f} €")
+
+    hc
+
+
+  })
+
+  
+  output$tbl_payoffs <- renderDT({
+    dat <- comp_data %>% mutate(across(where(is.numeric), ~round(., 2)))
+    datatable(dat, options = list(pageLength = 10))
+  })
+}
+
+shinyApp(ui, server)
---
+++ Functions.R
@@ -0,0 +1,205 @@
+
+
+# Simulation avec réinvestissement immédiat des dividendes
+simulate_investment_immediate <- function(stock_data, dividend_data, 
+                                          initial_investment, start_date, investment_period,
+                                          reinvest = TRUE, decote = 0.2) {
+  end_date = ymd(start_date) + years(investment_period)
+  
+  stock_data_      <- stock_data[paste0(start_date, "/", end_date)]
+  dividend_data_   <- dividend_data[paste0(start_date, "/", end_date)]
+  # split_data_      <- split_data[paste0(start_date, "/", end_date)]
+  
+  stock_data_$MeanPrice <- (Op(stock_data_) + Hi(stock_data_) + Lo(stock_data_) + Cl(stock_data_)) / 4
+  
+  
+  # achat initial
+  initial_price <- as.numeric(first(stock_data_$MeanPrice))
+  shares        <- initial_investment / (initial_price*(1-decote))
+  
+  # suivi de l’évolution du nombre d’actions
+  shares_evolution <- data.frame(Date = index(stock_data_), 
+                                 Shares = NA)
+  shares_evolution$Shares[1] <- shares
+  shares_evolution$Value[1]  <- initial_price
+  shares_evolution$TotalValue[1]  <- shares*initial_price
+  head(shares_evolution)
+  
+  # boucle journalière
+  for (i in 2:NROW(stock_data_)) {
+    d <- index(stock_data_)[i]
+    
+    # # appliquer split si présent
+    # if (d %in% index(split_data_)) {
+    #   ratio <- as.numeric(split_data_[d])
+    #   shares <- shares / ratio
+    # }
+    
+    # appliquer dividende si présent
+    if ((d %in% index(dividend_data_)) & reinvest==TRUE) {
+      dividend <- as.numeric(dividend_data_[d])
+      price    <- as.numeric(stock_data_$MeanPrice[i])
+      shares   <- shares + (dividend * shares) / price
+    }
+    
+    shares_evolution$Shares[i]     <- shares
+    shares_evolution$Value[i]      <- as.numeric(stock_data_$MeanPrice[i])
+    shares_evolution$TotalValue[i] <- shares*as.numeric(stock_data_$MeanPrice[i])
+  }
+  
+  return(shares_evolution)
+}
+
+simulate_investment_garantie <- function(stock_data, dividend_data, 
+                                         initial_investment, start_date, investment_period,
+                                         reinvest = TRUE, decote = 0.064) {
+  end_date        = ymd(start_date) + years(investment_period)
+  
+  stock_data_    <- stock_data[paste0(start_date, "/", end_date)]
+  start_date_real = first(index(stock_data_))
+  
+  dividend_data_ <- dividend_data[paste0(start_date, "/", end_date)]
+  
+  stock_data_$MeanPrice <- (Op(stock_data_) + Hi(stock_data_) + Lo(stock_data_) + Cl(stock_data_)) / 4
+  
+  # achat initial
+  initial_price    <- as.numeric(first(stock_data_$MeanPrice))
+  shares_total     <- initial_investment / (initial_price*(1-decote))
+  shares_employee  <- initial_investment / (initial_price)
+  shares_bank      <- shares_total-shares_employee 
+  
+  # suivi de l’évolution du nombre d’actions
+  shares_evolution <- data.frame(Date = index(stock_data_), 
+                                 SharesTotal    = NA,
+                                 #SharesEmployee = NA,
+                                 SharesBank     = NA,
+                                 Value          = NA,
+                                 TotalValueTotal    = NA,
+                                 TotalValueEmployee = NA,
+                                 TotalValueBank     = NA)
+  shares_evolution$SharesTotal[1]     <- shares_total
+  shares_evolution$SharesBank[1]      <- shares_bank
+  shares_evolution$Value[1]           <- initial_price
+  shares_evolution$TotalValueTotal[1]     <- shares_total*initial_price
+  shares_evolution$TotalValueEmployee[1]  <- initial_investment
+  shares_evolution$TotalValueBank[1]      <- shares_bank*initial_price
+  
+  # boucle journalière
+  for (i in 2:NROW(stock_data_)) {
+    d <- index(stock_data_)[i]
+    
+    # appliquer dividende si présent et réinvesti
+    if ((d %in% index(dividend_data_)) & reinvest==TRUE) {
+      dividend <- as.numeric(dividend_data_[d])
+      price    <- as.numeric(stock_data_$MeanPrice[i])
+      shares_total   <- shares_total + (dividend * shares_total) / price
+      shares_bank    <- shares_bank  + (dividend * shares_bank)  / price
+    }
+    
+    shares_evolution$SharesTotal[i]     <- shares_total
+    shares_evolution$SharesBank[i]      <- shares_bank
+    shares_evolution$Value[i]           <- as.numeric(stock_data_$MeanPrice[i])
+    shares_evolution$TotalValueTotal[i]     <- shares_total*as.numeric(stock_data_$MeanPrice[i])
+    shares_evolution$TotalValueBank[i]      <- shares_bank*as.numeric(stock_data_$MeanPrice[i])
+  }
+  
+  # Calcul des hausses protégées mensuelles 
+  start <- ymd(start_date_real)
+  end   <- ymd(end_date)
+  current <- start
+  monthly_prices <- c(initial_price)
+  monthly_dates  <- c()
+  
+  while (current < end) {
+    # subset des prix jusqu'au prochain mois
+    next_month      <- current %m+% months(1) - days(1)
+    prices_in_month <- stock_data_$MeanPrice[paste0(current, "/", next_month)]
+    
+    if (length(prices_in_month) > 0) {
+      monthly_prices <- c(monthly_prices, as.numeric(last(prices_in_month)))
+      monthly_dates  <- c(monthly_dates,  last(index(prices_in_month)))
+      # print(last(index(prices_in_month)))
+    }
+    
+    current <- current %m+% months(1)
+  }
+  monthly_dates = as.Date(monthly_dates)
+  monthly_dates = c(as.Date(start_date_real),monthly_dates)
+  
+  monthly_returns   <- monthly_prices - monthly_prices[1]
+  protected_returns <- pmax(monthly_returns, 0)
+  
+  #Payoff par pas de temps
+  avg_protected_return <- sapply(1:length(protected_returns), function(x) mean(protected_returns[1:x], na.rm=TRUE))
+  
+  payoff <- sapply(2:length(protected_returns), function(x) max(((1+0.03/12)^(x-1))*initial_investment,
+                                                                4*avg_protected_return[x]*shares_employee+initial_investment, na.rm=TRUE))
+  payoff <- c(initial_investment, payoff)
+  details_payoff = data.frame(Date                   = monthly_dates,
+                              PF_Cours_initial          = monthly_prices[1],
+                              PF_Cours_mois             = monthly_prices,
+                              PF_Return_protege_du_mois = protected_returns,
+                              PF_Return_protege_moy     = avg_protected_return,
+                              PF_Payoff                 = payoff)
+  
+  #Recap general
+  shares_evolution = merge(shares_evolution, details_payoff, by.x = c('Date'), by.y = c('Date'), all.x = TRUE)
+  shares_evolution[] <- lapply(shares_evolution, function(x) na.locf(x, na.rm = FALSE))
+  shares_evolution$TotalValueEmployee <- shares_evolution$PF_Payoff
+  
+  return(shares_evolution)
+}
+
+print(read_data)
+if(read_data){
+  stock_data    = getSymbols(ticker,    from = start_date, to = end_date, auto.assign = FALSE)
+  dividend_data = getDividends(ticker,  from = start_date, to = end_date)
+  
+  # séquence hebdomadaire de dates de départ
+  weekly_dates <- seq.Date(as.Date(start_date), as.Date(ymd(end_date) - years(5)), by = "year")
+  
+  classique_list <- lapply(weekly_dates, function(x) {
+    
+    sim <- simulate_investment_immediate(stock_data = stock_data, dividend_data = dividend_data,
+                                         initial_investment = initial_investment, start_date = x, investment_period = investment_period,
+                                         reinvest = TRUE, decote   = decote_classique)
+    last(sim)
+  })
+  # print(classique_list)
+  classique_ <- do.call(rbind, classique_list)
+  classique_$StartDate <- weekly_dates
+  
+  garantie_list <- lapply(weekly_dates, function(x) {
+    sim <- simulate_investment_garantie(stock_data = stock_data, dividend_data = dividend_data,
+                                        initial_investment = initial_investment, start_date = x, investment_period = investment_period,
+                                        reinvest = TRUE, decote   = decote_garantie)
+    last(sim)
+  })
+  garantie_ <- do.call(rbind, garantie_list)
+  garantie_$StartDate <- weekly_dates
+  
+  # construire table comparée
+  comp_data <- data.frame(StartDate = weekly_dates,
+                          Classique_TotalValue = classique_$TotalValue,
+                          Garantie_TotalValue  = garantie_$TotalValueTotal,
+                          Garantie_Employee    = garantie_$TotalValueEmployee)
+  comp_data$StartDate    <- as.Date(comp_data$StartDate)
+  
+  fwrite(stock_data,    "./Data/stock_data.csv")
+  fwrite(dividend_data, "./Data/dividend_data.csv")
+  fwrite(comp_data,     "./Data/comp_data.csv")
+}else{
+  print("go")
+  
+  stock_data    = read.csv("./Data/stock_data.csv")
+  stock_data    = xts(stock_data[,-1], order.by = as.Date(stock_data[,1]))
+  
+  dividend_data = read.csv("./Data/dividend_data.csv")
+  dividend_data = xts(dividend_data[,-1], order.by = as.Date(dividend_data[,1]))
+  
+  comp_data     = fread( "./Data/comp_data.csv")
+  # comp_data     = xts(comp_data[,-1], order.by = as.Date(comp_data[,1]))
+}
+
+
+
